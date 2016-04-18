d6650781ebe58f9e7e28f0278fde05539d25ef6d
John Kinzel <Germkiller42@gmail.com>
2016-04-18 13:20:50 -0500

    Set up Random Number Generation

--- DnDGenerator/Form1.Designer.cs
+++ DnDGenerator/Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Generate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(114, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(114, 335);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(114, 403);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(50, 448);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(173, 80);
+            this.Generate.TabIndex = 6;
+            this.Generate.Text = "Generate!";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 540);
+            this.Controls.Add(this.Generate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Generate;
     }
 }
 
--- DnDGenerator/Form1.cs
+++ DnDGenerator/Form1.cs
@@ -12,9 +12,67 @@ namespace DnDGenerator
 {
     public partial class Form1 : Form
     {
+        Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int[] Scores = new int[6];
+            int counter = 0;
+
+            while(counter < 6)
+            {
+                Scores[counter] = GenerateScores();
+
+                if(Scores[counter] >= 10)
+                {
+                    counter++;
+                }
+            }
+
+
+            label1.Text = Convert.ToString(Scores[0]);
+            label2.Text = Convert.ToString(Scores[1]);
+            label3.Text = Convert.ToString(Scores[2]);
+            label4.Text = Convert.ToString(Scores[3]);
+            label5.Text = Convert.ToString(Scores[4]);
+            label6.Text = Convert.ToString(Scores[5]);
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private int GenerateScores()
+        {
+            
+
+            int[] randomNumbers = new int[4];
+            randomNumbers[0] = rand.Next(0, 7);
+            randomNumbers[1] = rand.Next(0, 7);
+            randomNumbers[2] = rand.Next(0, 7);
+            randomNumbers[3] = rand.Next(0, 7);
+
+
+
+            Array.Sort(randomNumbers);
+
+            int randSum = randomNumbers[1] + randomNumbers[2] + randomNumbers[3];
+
+            return randSum;
+        }
+
+       
     }
 }
